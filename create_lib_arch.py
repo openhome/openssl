c4086a1c29d59210ae339a4a2496c3150fe6ea78
Gregg Hamilton <Gregg.Hamilton@linn.co.uk>
2013-06-07 12:10:57 +0100

    Made create_lib_arch more generic to build both Windows and Linux packages

--- create_lib_arch.py
+++ create_lib_arch.py
@@ -3,25 +3,35 @@
 import os
 import sys
 
-from create_lib import openssl, clean, create_package
+from create_lib import openssl, arch_opt_str, clean, create_package
 
-def build_linux(targets, ver):
+def build_arch(targets, ver):
     for arch in targets:
         clean(arch)
         create_package(['', arch, ver], targets)
 
 if __name__ == "__main__":
-    linux_arch = ['Linux-x86', 'Linux-x64', 'Linux-ARM', 'Core-armv6', 'Core-ppc32']
-    if (len(sys.argv) < 2):
-        print 'Usage: %s <version>' % (sys.argv[0])
+    base_arch = ['Windows', 'Linux']
+    windows_arch = ['Windows-x86', 'Windows-x64']
+    linux_arch = ['Linux-x86', 'Linux-x64', 'Core-armv6', 'Core-ppc32']#, 'Linux-ARM']
+    if (len(sys.argv) < 3) or (sys.argv[1] not in base_arch):
+        availArchStr = arch_opt_str(base_arch)
+        print 'Usage: %s (%s) <version>' % (sys.argv[0], availArchStr)
         exit(1)
 
+    argBaseArch = sys.argv[1]
+    argVer      = sys.argv[2]
+    targetArch  = ''
     try:
         os.chdir(openssl)
     except OSError:
         print 'Error: Unable to change to dir:', openssl
         exit(1)
-    build_linux(linux_arch, sys.argv[1])
+    if (argBaseArch == 'Windows'):
+        targetArch = windows_arch
+    else:
+        targetArch = linux_arch
+    build_arch(targetArch, argVer)
     try:
         os.chdir('..')
     except OSError:
